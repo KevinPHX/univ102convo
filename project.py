d270d248f6d8809a9cdc2ea290c5d30ba5780721
Kevin Yin <ky96@duke.edu>
2023-12-02 16:02:40 -0500

    push

--- project.py
+++ project.py
@@ -16,13 +16,14 @@ st.markdown('Source: Effrosynidis, Dimitrios, Alexandros I. Karasakalidis, Georg
 # df = pd.read_csv('The Climate Change Twitter Dataset.csv')
 @st.cache_data  # 👈 Add the caching decorator
 def load_data():
-    df = pd.read_csv('data.csv', engine="pyarrow")
+    df = pd.read_csv('alt_data.csv.zip', engine="pyarrow")
+    df['year'] = pd.DatetimeIndex(df['created_at']).year
+    df['date'] = pd.DatetimeIndex(df['created_at']).normalize()
+    df['created_at'] = pd.to_datetime(df['created_at'])
     return df
 df = load_data()
 st.write(df.columns)
-df['year'] = pd.DatetimeIndex(df['created_at']).year
-df['date'] = pd.DatetimeIndex(df['created_at']).normalize()
-df['created_at'] = pd.to_datetime(df['created_at'])
+
 
 
 def get_relevant(start = df.date.min(), end = df.date.max()):
